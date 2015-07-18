8b93d403254a36d160b8ffddd28e4b93f1eb9f01
zempon <zempon@vt.edu>
2015-07-18 15:11:13 -0400

    Updates to add new classes, object, continer character and so on.

---
+++ core/src/HorizonConfluct/entity.java
@@ -0,0 +1,7 @@
+package HorizonConfluct;
+import java.util.List;
+import java.util.ArrayList;
+
+public class entity {
+	private ArrayList<ComponentPorts> components = new ArrayList<ComponentPorts>();
+}
